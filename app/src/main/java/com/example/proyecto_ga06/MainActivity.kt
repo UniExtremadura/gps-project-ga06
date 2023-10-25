8c7e2f4b81eac190d9cb52cf408e540824b5d6b1
AngelaAlvarezMartin <aalvarezyp@alumnos.unex.es>
2023-10-25 13:27:28 +0200

    wip

--- app/src/main/java/com/example/proyecto_ga06/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_ga06/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.appcompat.app.AppCompatActivity
+
+
+
 import com.example.proyecto_ga06.ui.theme.Proyecto_ga06Theme
 
 class MainActivity : ComponentActivity() {
